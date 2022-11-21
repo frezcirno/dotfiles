084628cadf488006dc53634cc3a22c2615f99135
frezcirno <2277861660@qq.com>
2022-11-22 00:41:57 +0800

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+# MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+MONITOR=eDP-1-1
 
 # Launch bar1 and bar2
-MONITOR=$MONITOR exec polybar main >/tmp/polybar.log 2>&1
+exec polybar main >/tmp/polybar.log 2>&1
