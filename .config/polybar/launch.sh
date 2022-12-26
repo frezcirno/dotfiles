2fca33a31f521c7f36e52ada38cd3a0b834e5a29
frezcirno <2277861660@qq.com>
2022-12-26 12:41:29 +0800

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Terminate already running bar instances
-killall -q polybar
+killall -9 polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
-MONITOR=eDP-1-1
+export MONITOR=$(polybar -m|head -1|sed -e 's/:.*$//g')
+# export MONITOR=DP-0
 
 # Launch bar1 and bar2
-exec polybar main >/tmp/polybar.log 2>&1
+exec polybar main >$DIR/polybar.log 2>&1
